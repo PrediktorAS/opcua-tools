8da74a093524dd911cdd437a56c0ceec3aca4c18
Magnus Bakken <magbak@gmail.com>
2021-08-11 11:28:08 +0200

    Expose another function

--- opcua_tools/__init__.py
+++ opcua_tools/__init__.py
@@ -14,6 +14,6 @@
 
 from .navigation import *
 from .nodeset_generator import create_nodeset2_file, validate_nodeset2_file
-from .nodeset_parser import parse_xml_dir, parse_xml
+from .nodeset_parser import parse_xml_dir, parse_xml, normalize_wrt_nodeid
 from .instance_signature import fully_inherited_instance_declarations
 from .ua_data_types import *
