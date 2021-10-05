e37f340a6088041903b3f661bc32f3528a43405b
Olav Pedersen <ope@prediktor.no>
2021-10-05 21:15:15 +0200

    Added a function for get_enum_int from string

--- opcua_tools/ua_graph.py
+++ opcua_tools/ua_graph.py
@@ -191,3 +191,24 @@ class UAGraph:
         enum_dict = self.get_enum_dict(enum_name)
         #TODO: Cache enumdict for performance
         return enum_dict[number]
+
+    def get_enum_int(self, enum_name: str, string: str):
+        """This function will return the integer value of within an enum,
+        provided you get both the of the enum and a string value."""
+        # Ad Hoc solution to ensure text in files does not include newlines and line breaks
+        string = string.replace("\r", "")
+        string = string.replace("\n", "")
+        enum_dict = self.get_enum_dict(enum_name)
+        # TODO: Cache enumdict for performance
+        # Getting the dict key based on the value
+        key_list = list(enum_dict.keys())
+        val_list = list(enum_dict.values())
+        try:
+            position = val_list.index(string)
+            return key_list[position]
+        except:
+            raise ValueError(
+                "Could not find the string: {}, for the enum_name: {}".format(
+                    string, enum_name
+                )
+            )
