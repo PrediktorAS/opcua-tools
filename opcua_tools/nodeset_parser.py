ab8bd17a5e3981e9175a225f906224c09b624c9b
Andreas Lydersen <andreas.lydersen@prediktor.com>
2021-10-07 10:11:49 +0200

    Fixed error that made tests fail

--- opcua_tools/nodeset_parser.py
+++ opcua_tools/nodeset_parser.py
@@ -52,7 +52,7 @@ def findval(elem, uaxsd):
 def finddisplayname(elem, uaxsd):
     val = elem.find(uaxsd + 'DisplayName')
     if val is None:
-        return None
+        return ''
 
     if val.text is None:
         return ''
@@ -63,7 +63,7 @@ def finddisplayname(elem, uaxsd):
 def finddescription(elem, uaxsd):
     val = elem.find(uaxsd + 'Description')
     if val is None:
-        return None
+        return ''
 
     if val.text is None:
         return ''
@@ -308,23 +308,22 @@ def parse_xml_without_normalization(xmlfile: Union[str, BytesIO], namespaces: Op
     return {'nodes': nodes, 'references': references, 'namespaces':namespaces}
 
 
-def parse_xml_dir(xmldir: str, namespaces: Optional[List[str]]=None) -> Dict[str, Any]:
-    if namespaces is None:
-        namespaces = []
-
+def parse_xml_dir(xmldir: str, namespaces: Optional[List[str]]=[]) -> Dict[str, Any]:
     df_nodes_list = []
     df_references_list = []
     files = [file for file in os.listdir(xmldir)]
     files.sort()
     for file in files:
-        if file.endswith(".xml"):
-            logger.info('Started parsing ' + str(file))
-            xmlfile = xmldir + '/' + file
-            parse_dict = parse_xml_without_normalization(xmlfile, namespaces)
-            namespaces = parse_dict['namespaces']
-            df_nodes_list.append(parse_dict['nodes'])
-            df_references_list.append((parse_dict['references']))
-            logger.info('Finished parsing ' + str(file))
+        if not file.endswith(".xml"):
+            continue
+
+        logger.info('Started parsing ' + str(file))
+        xmlfile = os.path.join(xmldir, file)
+        parse_dict = parse_xml_without_normalization(xmlfile, namespaces)
+        namespaces = parse_dict['namespaces']
+        df_nodes_list.append(parse_dict['nodes'])
+        df_references_list.append((parse_dict['references']))
+        logger.info('Finished parsing ' + str(file))
 
     nodes = pd.concat(df_nodes_list)
     references = pd.concat(df_references_list)
