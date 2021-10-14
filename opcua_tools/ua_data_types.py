72b0ee3d0fdff8ba09888409872fab044f335cc0
Magnus Bakken <magbak@gmail.com>
2021-10-14 22:08:55 +0200

    Oneline values

--- opcua_tools/ua_data_types.py
+++ opcua_tools/ua_data_types.py
@@ -20,6 +20,7 @@ from base64 import b64encode
 from enum import Enum
 from xml.sax.saxutils import escape
 
+
 class NodeIdType(Enum):
     NUMERIC = 'i'
     STRING = 's'
@@ -29,18 +30,23 @@ class NodeIdType(Enum):
 
 UAXMLNS_ATTRIB = 'xmlns="http://opcfoundation.org/UA/2008/02/Types.xsd"'
 
+
 @dataclass(eq=True, frozen=True)
 class UAData(ABC):
     pass
 
     @abstractmethod
-    def xml_encode(self, include_xmlns : bool) -> str:
+    def xml_encode(self, include_xmlns: bool) -> str:
         pass
 
     def __lt__(self, other): return lt(self, other)
+
     def __gt__(self, other): return gt(self, other)
+
     def __le__(self, other): return le(self, other)
+
     def __ge__(self, other): return ge(self, other)
+
 
 @dataclass(eq=True, frozen=True)
 class DataTypeField(UAData):
@@ -48,7 +54,7 @@ class DataTypeField(UAData):
     value: str
     description: str
 
-    def xml_encode(self, include_xmlns:bool) -> str:
+    def xml_encode(self, include_xmlns: bool) -> str:
         x = '<Field Name="' + self.name + '" Value="' + self.value + '"'
         if include_xmlns:
             x += ' ' + UAXMLNS_ATTRIB
@@ -57,12 +63,13 @@ class DataTypeField(UAData):
         x += '</Field>'
         return x
 
+
 @dataclass(eq=True, frozen=True)
 class DataTypeDefinition(UAData):
     name: str
     fields: Tuple[DataTypeField]
 
-    def xml_encode(self, include_xmlns:bool) -> str:
+    def xml_encode(self, include_xmlns: bool) -> str:
         encodedvalues = [v.xml_encode(include_xmlns=False) for v in self.fields]
         x = '<Definition Name="' + self.name + '"'
         if include_xmlns:
@@ -75,23 +82,26 @@ class DataTypeDefinition(UAData):
 class UABuiltIn(UAData):
     pass
 
+
 @dataclass(eq=True, frozen=True)
-class UAInteger(UABuiltIn): #TODO make unsigned / signed integers type
+class UAInteger(UABuiltIn):  # TODO make unsigned / signed integers type
     value: Optional[int]
+
 
 @dataclass(eq=True, frozen=True)
 class UASByte(UAInteger):
 
-    def xml_encode(self, include_xmlns:bool) -> str:
+    def xml_encode(self, include_xmlns: bool) -> str:
         x = '<SByte'
         if include_xmlns:
             x += ' ' + UAXMLNS_ATTRIB
         x += '>' + (str(self.value) if self.value is not None else '') + '</SByte>'
         return x
 
+
 @dataclass(eq=True, frozen=True)
 class UAByte(UAInteger):
-    
+
     def xml_encode(self, include_xmlns: bool) -> str:
         x = '<UAByte'
         if include_xmlns:
@@ -99,9 +109,10 @@ class UAByte(UAInteger):
         x += '>' + (str(self.value) if self.value is not None else '') + '</UAByte>'
         return x
 
+
 @dataclass(eq=True, frozen=True)
 class UAInt16(UAInteger):
-    
+
     def xml_encode(self, include_xmlns: bool) -> str:
         x = '<Int16'
         if include_xmlns:
@@ -109,9 +120,10 @@ class UAInt16(UAInteger):
         x += '>' + (str(self.value) if self.value is not None else '') + '</Int16>'
         return x
 
+
 @dataclass(eq=True, frozen=True)
 class UAUInt16(UAInteger):
-    
+
     def xml_encode(self, include_xmlns: bool) -> str:
         x = '<UInt16'
         if include_xmlns:
@@ -119,9 +131,10 @@ class UAUInt16(UAInteger):
         x += '>' + (str(self.value) if self.value is not None else '') + '</UInt16>'
         return x
 
+
 @dataclass(eq=True, frozen=True)
 class UAInt32(UAInteger):
-    
+
     def xml_encode(self, include_xmlns: bool) -> str:
         x = '<Int32'
         if include_xmlns:
@@ -129,15 +142,17 @@ class UAInt32(UAInteger):
         x += '>' + (str(self.value) if self.value is not None else '') + '</Int32>'
         return x
 
+
 @dataclass(eq=True, frozen=True)
 class UAUInt32(UAInteger):
-    
+
     def xml_encode(self, include_xmlns: bool) -> str:
         x = '<UInt32'
         if include_xmlns:
             x += ' ' + UAXMLNS_ATTRIB
         x += '>' + (str(self.value) if self.value is not None else '') + '</UInt32>'
         return x
+
 
 @dataclass(eq=True, frozen=True)
 class UAInt64(UAInteger):
@@ -148,6 +163,7 @@ class UAInt64(UAInteger):
         x += '>' + (str(self.value) if self.value is not None else '') + '</Int64>'
         return x
 
+
 @dataclass(eq=True, frozen=True)
 class UAUInt64(UAInteger):
     def xml_encode(self, include_xmlns: bool) -> str:
@@ -157,9 +173,11 @@ class UAUInt64(UAInteger):
         x += '>' + (str(self.value) if self.value is not None else '') + '</UInt64>'
         return x
 
+
 @dataclass(eq=True, frozen=True)
 class UAFloatingPoint(UABuiltIn):
     value: Optional[float]
+
 
 @dataclass(eq=True, frozen=True)
 class UAFloat(UAFloatingPoint):
@@ -170,6 +188,7 @@ class UAFloat(UAFloatingPoint):
         x += '>' + (str(self.value) if self.value is not None else '') + '</Float>'
         return x
 
+
 @dataclass(eq=True, frozen=True)
 class UADouble(UAFloatingPoint):
     def xml_encode(self, include_xmlns: bool) -> str:
@@ -179,16 +198,18 @@ class UADouble(UAFloatingPoint):
         x += '>' + (str(self.value) if self.value is not None else '') + '</Double>'
         return x
 
+
 @dataclass(eq=True, frozen=True)
 class UAString(UABuiltIn):
     value: Optional[str]
-    
+
     def xml_encode(self, include_xmlns: bool) -> str:
         x = '<String'
         if include_xmlns:
             x += ' ' + UAXMLNS_ATTRIB
         x += '>' + (escape(str(self.value)) if self.value is not None else '') + '</String>'
         return x
+
 
 @dataclass(eq=True, frozen=True)
 class UADateTime(UABuiltIn):
@@ -201,9 +222,11 @@ class UADateTime(UABuiltIn):
         x += '>' + (self.value.isoformat() if self.value is not None else '') + '</DateTime>'
         return x
 
+
 @dataclass(eq=True, frozen=True)
 class UAGuid(UAString):
     pass
+
 
 @dataclass(eq=True, frozen=True)
 class UAByteString(UABuiltIn):
@@ -216,6 +239,7 @@ class UAByteString(UABuiltIn):
         x += '>' + (b64encode(self.value).decode('utf-8') if self.value is not None else '') + '</ByteString>'
         return x
 
+
 @dataclass(eq=True, frozen=True)
 class UABoolean(UABuiltIn):
     value: Optional[bool]
@@ -227,9 +251,11 @@ class UABoolean(UABuiltIn):
         x += '>' + (('true' if self.value is True else 'false') if self.value is not None else '') + '</Boolean>'
         return x
 
+
 @dataclass(eq=True, frozen=True)
 class UAXMLElement(UABuiltIn):
     pass
+
 
 @dataclass(eq=True, frozen=True)
 class UANodeId(UAData):
@@ -250,21 +276,26 @@ class UANodeId(UAData):
         x += '>' + self.__str__() + '</Identifier>'
         return x
 
+
 @dataclass(eq=True, frozen=True)
 class UAExpandedNodeId(UAString):
     value: Optional[str]
+
 
 @dataclass(eq=True, frozen=True)
 class UAStatusCode(UABuiltIn):
     value: int
 
+
 @dataclass(eq=True, frozen=True)
 class UADiagnosticInfo(UABuiltIn):
     pass
 
+
 @dataclass(eq=True, frozen=True)
 class UAQualifiedName(UABuiltIn):
     pass
+
 
 @dataclass(eq=True, frozen=True)
 class UALocalizedText(UABuiltIn):
@@ -278,39 +309,47 @@ class UALocalizedText(UABuiltIn):
         x += '>\n'
         x += '<Locale>' + (self.locale if self.locale is not None else '') + '</Locale>\n'
         x += '<Text>' + (escape(self.text) if self.text is not None else '') + '</Text>\n'
-        x +='</LocalizedText>'
+        x += '</LocalizedText>'
         return x
+
 
 @dataclass(eq=True, frozen=True)
 class UAVariant(UABuiltIn):
     pass
 
+
 @dataclass(eq=True, frozen=True)
 class UADataValue(UABuiltIn):
     pass
+
 
 @dataclass(eq=True, frozen=True)
 class UADecimal(UAData):
     pass
 
+
 @dataclass(eq=True, frozen=True)
 class UAEnumeration(UAData):
     pass
+
 
 @dataclass(eq=True, frozen=True)
 class UAArray(UAData):
     pass
 
+
 @dataclass(eq=True, frozen=True)
 class UAStructure(UAData):
     xmlstring: str
 
-    def xml_encode(self, include_xmlns : bool) -> str:
+    def xml_encode(self, include_xmlns: bool) -> str:
         return self.xmlstring
+
 
 @dataclass(eq=True, frozen=True)
 class UAStructureOptionalField(UAData):
     pass
+
 
 @dataclass(eq=True, frozen=True)
 class UnparsedUAExtensionObject(UABuiltIn):
@@ -322,10 +361,12 @@ class UnparsedUAExtensionObject(UABuiltIn):
         if include_xmlns:
             x += ' ' + UAXMLNS_ATTRIB
         x += '>\n'
-        x += '<TypeId>' + (self.type_nodeid.xml_encode(include_xmlns=False) if self.type_nodeid is not None else '') + '</TypeId>\n'
+        x += '<TypeId>' + (
+            self.type_nodeid.xml_encode(include_xmlns=False) if self.type_nodeid is not None else '') + '</TypeId>\n'
         x += '<Body>' + (self.body.xml_encode(include_xmlns=False) if self.body is not None else '') + '</Body>\n'
-        x +='</ExtensionObject>'
+        x += '</ExtensionObject>'
         return x
+
 
 @dataclass(eq=True, frozen=True)
 class UAEngineeringUnits(UAData):
@@ -336,36 +377,39 @@ class UAEngineeringUnits(UAData):
 
     def xml_encode(self, include_xmlns: bool) -> str:
         body_contents = '<EngineeringUnits>'
-        body_contents += '<NamespaceUri>' + self.namespace_uri + '</NamespaceUri>\n'
-        body_contents += '<UnitId>' + str(self.unit_id) + '</UnitId>\n'
+        body_contents += '<NamespaceUri>' + self.namespace_uri + '</NamespaceUri>'
+        body_contents += '<UnitId>' + str(self.unit_id) + '</UnitId>'
         body_contents += '<DisplayName>'
-        body_contents += '<Locale>' + (self.display_name.locale if self.display_name.locale is not None else '') + '</Locale>\n'
-        body_contents += '<Text>' + (self.display_name.text if self.display_name.text is not None else '') + '</Text>\n'
-        body_contents += '</DisplayName>\n'
+        body_contents += '<Locale>' + (
+            self.display_name.locale if self.display_name.locale is not None else '') + '</Locale>'
+        body_contents += '<Text>' + (self.display_name.text if self.display_name.text is not None else '') + '</Text>'
+        body_contents += '</DisplayName>'
         body_contents += '<Description>'
-        body_contents += '<Locale>' + (self.description.locale if self.description.locale is not None else '') + '</Locale>\n'
-        body_contents += '<Text>' + (self.description.text if self.description.text is not None else '') + '</Text>\n'
-        body_contents += '</Description>\n'
+        body_contents += '<Locale>' + (
+            self.description.locale if self.description.locale is not None else '') + '</Locale>'
+        body_contents += '<Text>' + (self.description.text if self.description.text is not None else '') + '</Text>'
+        body_contents += '</Description>'
         body_contents += '</EngineeringUnits>'
 
-        #Todo fix hacky implementation
+        # Todo fix hacky implementation
         return UnparsedUAExtensionObject(UANodeId(0, NodeIdType.NUMERIC, '888'),
                                          UAStructure(xmlstring=body_contents)).xml_encode(include_xmlns)
-
-
 
 
 @dataclass(eq=True, frozen=True)
 class UAUnion(UAData):
     pass
 
+
 @dataclass(eq=True, frozen=True)
 class UAMessage(UAData):
     pass
 
+
 @dataclass(eq=True, frozen=True)
 class UAEmpty(UAData):
     pass
+
 
 @dataclass(eq=True, frozen=True)
 class UAListOf(UAData):
@@ -380,11 +424,13 @@ class UAListOf(UAData):
         x = '<ListOf' + self.typename + ' '
         if include_xmlns:
             x += ' ' + UAXMLNS_ATTRIB
-        x += '>\n' + '\n'.join(encodedvalues) + '\n</ListOf' + self.typename + '>'
+        x += '>' + ''.join(encodedvalues) + '</ListOf' + self.typename + '>'
         return x
+
 
 def ge(u1: UAData, u2: UAData):
     return le(u1=u2, u2=u1)
+
 
 def le(u1: UAData, u2: UAData):
     if u2 is None:
@@ -399,8 +445,10 @@ def le(u1: UAData, u2: UAData):
 
     return t1 <= t2
 
-def gt(u1: UAData, u2:UAData):
+
+def gt(u1: UAData, u2: UAData):
     return lt(u1=u2, u2=u1)
+
 
 def lt(u1: UAData, u2: UAData):
     if u2 is None:
