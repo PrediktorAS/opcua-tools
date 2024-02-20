8cecebe25154e6e443a0d3b2056b6eac9597a9e8
Olav Pedersen <ope@prediktor.no>
2024-02-20 15:16:11 +0100

    Bumped version number

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="opcua-tools",
-    version="1.4.3",
+    version="1.5.3",
     description="OPCUA Tools for Python using Pandas DataFrames",
     long_description=README,
     long_description_content_type="text/markdown",
