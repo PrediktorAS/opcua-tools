c6da8329fbea668aa31e374476baac4e98ebbbb1
Dawid Makar <dawid.makar@acaisoft.com>
2024-06-28 12:04:51 +0200

    Fix get_namespace_data_from_file. It shouldn't rely on OPC UA Nodeset2 XML file name

--- tests/test_nodeset_parser.py
+++ tests/test_nodeset_parser.py
@@ -23,3 +23,20 @@ def test_get_namespace_data_from_file():
         },
         "name": "http://opcfoundation.org/UA/IEC61850-7-4",
     }
+
+
+def test_get_namespaca_data_should_have_empty_namespaces_data_regardless_file_name():
+    example_xml_path = os.path.join(
+        get_project_root(),
+        "tests",
+        "testdata",
+        "parser",
+        "Opc.Ua.NodeSet2.Services.xml",
+    )
+
+    ns_data = nodeset_parser.get_namespace_data_from_file(example_xml_path)
+
+    assert ns_data == {
+        "included_namespaces": set(),
+        "name": "http://opcfoundation.org/UA/",
+    }
