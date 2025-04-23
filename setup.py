b5923e774a81041ae1739245dab63a2fbbaf68fa
Dawid Makar <dawid.makar@acaisoft.com>
2025-04-23 16:24:17 +0200

    PENGSOL-764: Fix UAGraph creation when a nodeset2 file with no nodes defined is given

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="opcua-tools",
-    version="1.8.1",
+    version="1.8.2",
     description="OPCUA Tools for Python using Pandas DataFrames",
     long_description=README,
     long_description_content_type="text/markdown",
